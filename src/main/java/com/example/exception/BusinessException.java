fa034cd573c152cd49b88bf58e7353812460cdd3
ko-aoki <ko.aoki1223@gmail.com>
2017-03-08 23:22:18 +0900

    業務エラー追加

---
+++ src/main/java/com/example/exception/BusinessException.java
@@ -0,0 +1,28 @@
+package com.example.exception;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 業務例外クラス.
+ */
+public class BusinessException extends RuntimeException {
+
+    /** メッセージリスト */
+    private List<String> messageList;
+
+    public BusinessException(String message) {
+        this.messageList = Arrays.asList(message);
+    }
+
+    public BusinessException( List<String> messageList) {
+        this.messageList = messageList;
+    }
+
+    /**
+     * メッセージリストを返します.
+     */
+    public List<String> getMessageList() {
+        return this.messageList;
+    }
+}
