4499352a8402366ced0e343e8be1570d74c6d279
ko-aoki <ko.aoki1223@gmail.com>
2018-02-28 08:22:50 +0900

    開発時schema,sql用の設定誤りを修正

--- src/main/java/com/example/web/TopController.java
+++ src/main/java/com/example/web/TopController.java
@@ -1,10 +1,8 @@
 package com.example.web;
 
-import com.example.security.LoginUserDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
